96e012499d1f3d427b14360ef3280c51c2c3ca18
Bill <jfg@163.com>
2018-06-13 00:14:37 +0800

    resLog

--- apps/http/index.js
+++ apps/http/index.js
@@ -11,12 +11,20 @@ const cfg = config[env];
 
 const log = log4js.getLogger();
 log.level = 'debug';
+const resLog = async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    const bytes = ctx.response.header['content-length'] || 0;
+    log.info(`${ctx.method} ${ctx.url} ${ctx.response.status} ${bytes}bytes ${ms}ms`);
+}
 
 const normalizePort = val => (parseInt(val));
 const port = normalizePort(process.env.PORT || cfg.port || 3000);
 
 const router = require('./routes');
 const app = new Koa();
+app.use(resLog);
 app.use(static(path.join(global.dir, 'static')));
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(port);
