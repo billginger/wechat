95b26a500fe99483f5f8813d5bf9ceef7c0b302b
Bill <jfg@163.com>
2018-06-12 23:23:03 +0800

    use log4js

--- apps/http/index.js
+++ apps/http/index.js
@@ -3,10 +3,15 @@ global.dir = __dirname;
 const path = require('path');
 const Koa = require('koa');
 const static = require('koa-static');
+const log4js = require('log4js');
 
 const config = require('./config.js');
 const env = process.env.NODE_ENV || 'local';
 const cfg = config[env];
+
+const log = log4js.getLogger();
+log.level = 'debug';
+
 const normalizePort = val => (parseInt(val));
 const port = normalizePort(process.env.PORT || cfg.port || 3000);
 
@@ -16,4 +21,4 @@ app.use(static(path.join(global.dir, 'static')));
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(port);
 
-console.log('WeChat HTTP is starting at port ' + port);
+log.info('WeChat HTTP is starting at port ' + port);
